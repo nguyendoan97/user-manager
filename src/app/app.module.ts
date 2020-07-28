082dd5bca4179457f7bdc073bb6182489413344b
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-07-28 17:02:26 +0700

    fix hiển thị ô search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavComponent} from './menu/nav/nav.component';
 import {UserComponent} from './list/user/user.component';
+import {InputSearchComponent} from './list/input-search/input-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    UserComponent
+    UserComponent,
+    InputSearchComponent
   ],
   imports: [
     BrowserModule,
